d5db74893587fffa1b3d5665f4c1b9a368a25153
Gabriel Lanata <gabriel@lanata.me>
2025-01-02 17:30:42 -0800

    open ai auth

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,7 @@ alias aliases="$EDITOR ~/dotfiles/aliases.zsh"
 # 1password auth
 export OP_PLUGIN_ALIASES_SOURCED=1
 alias gh="op plugin run -- gh"
+alias openai="op plugin run -- openai"
 alias sentry-cli="op plugin run -- sentry-cli"
 
 # system
