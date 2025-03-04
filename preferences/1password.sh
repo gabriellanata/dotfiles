bb6d2c692e049488296b4dd1b0949c5ff1d48188
Gabriel Lanata <gabriel@lanata.me>
2025-03-04 01:06:44 -0800

    Cleanup

--- preferences/1password.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-link_file "$DOTFILES_DIR/configs/1password/settings.json" "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/Library/Application Support/1Password/Data/settings/settings.json"
